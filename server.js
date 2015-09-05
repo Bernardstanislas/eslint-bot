03eadf9e4cc017c59445e648e9887fe44aeb7125
Stanislas Bernard <bernardstanislas@gmail.com>
2015-09-05 14:35:42 +0200

    Fix typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/', (request, response) => {
-    console.ingo('Got request', request);
+    console.info('Got request', request);
     response.end();
 });
 
