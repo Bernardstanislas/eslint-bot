afe75ee22fa0280b06f23a99b243ffa10f255085
Stanislas Bernard <bernardstanislas@gmail.com>
2015-09-06 23:15:46 +0200

    Add comments

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Dependencies
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,18 +8,22 @@ const _ = require('lodash');
 const request = require('request');
 const ESLintCLIEngine = require('eslint').CLIEngine;
 
+// Config variables
+
+import {GITHUB_USERNAME, GITHUB_PASSWORD, REPOSITORY_OWNER, REPOSITORY_NAME, FILE_FILTER} from './config';
+
 // Github configuration
 
 const github = new GitHubApi({
     version: '3.0.0',
     headers: {
-        'user-agent': 'Github-linter' // GitHub is happy with a unique user agent
+        'user-agent': 'ESLint-bot' // GitHub is happy with a unique user agent
     }
 });
 github.authenticate({
     type: 'basic',
-    username: process.env.GITHUB_USERNAME,
-    password: process.env.GITHUB_PASSWORD
+    username: GITHUB_USERNAME,
+    password: GITHUB_PASSWORD
 });
 
 // Eslint configuration
@@ -26,26 +32,63 @@ const eslint = new ESLintCLIEngine();
 
 // Functions
 
+/**
+ * Get commits from a payload object.
+ * @param  {Object} payload         the payload sent by Github
+ * @return {Array} the commits pushed to Github
+ */
 const getCommitsFromPayload = payload => payload.commits;
 
-const getFilesFromCommit = (callback, {id: sha}, {user, repo}) => {
+/**
+ * Get modified files from a commit.
+ * @param  {Function} callback      callback called when the files are fetched
+ * @param  {Object}   {id}          the commit object
+ */
+const getFilesFromCommit = (callback, {id: sha}) => {
     github.repos.getCommit({
-        user,
-        repo,
+        user: REPOSITORY_OWNER,
+        repo: REPOSITORY_NAME,
         sha
     }, (error, {files}) => {
+        if (error) {
+            console.log(error);
+        }
         callback(files, sha);
     });
 };
 
-const filterJavascriptFiles = files => files.filter(({filename}) => filename.match(/\.(js|jsx)$/) && filename.match(/^src.*/));
+/**
+ * Filter files to keep only Javascript files. ESLint is for Javascript. No kidding.
+ * @param  {Array} files        every files contained in the commit
+ * @return {Array} Filtered files, which matched the FILE_FILTER regex (set in the config)
+ */
+const filterJavascriptFiles = files => files.filter(({filename}) => filename.match(FILE_FILTER));
 
-const downloadFile = (callback, {filename, patch, raw_url}, sha, config) => {
+/**
+ * Download a file from its url, then call the callback with its content.
+ * @param  {Function} callback          Download success callback
+ * @param  {String}   filename          File filename
+ * @param  {String}   patch             The commit's patch string.
+ * @param  {String}   raw_url           File URL
+ * @param  {String}   sha               Commit id
+ */
+const downloadFile = (callback, {filename, patch, raw_url}, sha) => { // eslint-disable-line
     request(raw_url, (error, response, content) => {
-        callback(filename, patch, content, sha, config);
+        if (error) {
+            console.log(error);
+        }
+        callback(filename, patch, content, sha);
     });
 };
 
+/**
+ * Compute a mapping object for the relationship 'file line number' <-> 'Github's diff view line number'.
+ * This is necessary for the comments, as Github API asks to specify the line number in the diff view to attach an inline comment to.
+ * If a file line is not modified, then it will not appear in the diff view, so it is not taken into account here.
+ * The linter will therefore only mention warnings for modified lines.
+ * @param  {String}   patchString               The git patch string.
+ * @return {Object} An object shaped as follows : {'file line number': 'diff view line number'}.
+ */
 const getLineMapFromPatchString = patchString => {
     let diffLineIndex = 0;
     let fileLineIndex = 0;
@@ -65,36 +108,66 @@ const getLineMapFromPatchString = patchString => {
     }, {});
 };
 
-const lintContent = (filename, patch, content, sha, config) => {
-    return {filename, lineMap: getLineMapFromPatchString(patch), messages: _.get(eslint.executeOnText(content, filename), 'results[0].messages'), sha, config};
+/**
+ * Lint a raw content passed as a string, then return the linting messages.
+ * @param  {String} filename File filename
+ * @param  {String} patch    Commit's Git patch
+ * @param  {String} content  File content
+ * @param  {String} sha      Commit's id
+ * @return {Array}  Linting messages
+ */
+const lintContent = (filename, patch, content, sha) => {
+    return {filename, lineMap: getLineMapFromPatchString(patch), messages: _.get(eslint.executeOnText(content, filename), 'results[0].messages'), sha};
 };
 
-const sendSingleComment = (filename, lineMap, {ruleId='Eslint', message, line}, sha, {user, repo}) => {
+/**
+ * Send a comment to Github's commit view
+ * @param  {String} filename File filename
+ * @param  {Object} lineMap  The map between file and diff view line numbers
+ * @param  {String} ruleId ESLint rule id
+ * @param  {String} message  ESLint message
+ * @param  {Integer} line  Line number (in the file)
+ * @param  {String} sha      Commit's id
+ */
+const sendSingleComment = (filename, lineMap, {ruleId='Eslint', message, line}, sha) => {
     const diffLinePosition = lineMap[line];
-    if (diffLinePosition) {
+    if (diffLinePosition) { // By testing this, we skip the linting messages related to non-modified lines.
         github.repos.createCommitComment({
-            user,
-            repo,
+            user: REPOSITORY_OWNER,
+            repo: REPOSITORY_NAME,
             sha,
             path: filename,
-            commit_id: sha,
+            commit_id: sha, // eslint-disable-line
             body: `**${ruleId}**: ${message}`,
             position: diffLinePosition
         });
     }
 };
 
-const sendComments = ({filename, lineMap, messages, sha, config}) => {
-    messages.map(message => sendSingleComment(filename, lineMap, message, sha, config));
+/**
+ * Send the comments for all the linting messages, to Github
+ * @param  {String} filename File filename
+ * @param  {Object} lineMap  The map between file and diff view line numbers
+ * @param  {Array} messages  ESLint messages
+ * @param  {String} sha      Commit's id
+ */
+const sendComments = ({filename, lineMap, messages, sha}) => {
+    messages.map(message => sendSingleComment(filename, lineMap, message, sha));
 };
 
-const treatPayload = (config, payload) => {
+/**
+ * Main function, that treats the payload sent by Github.
+ * First it gets the commits, the it extracts the filenames from the commit, downloads and filters the files.
+ * The remaining files are analyzed by the linter, and the resulting linting messages are sent to Github as inline comments.
+ * @param  {Object} payload Push event's payload sent by Github.
+ */
+const treatPayload = payload => {
     getCommitsFromPayload(payload).map(commit => {
         getFilesFromCommit((files, sha) => {
             filterJavascriptFiles(files).map(file => {
-                downloadFile(_.compose(sendComments, lintContent), file, sha, config);
+                downloadFile(_.compose(sendComments, lintContent), file, sha);
             });
-        }, commit, config);
+        }, commit);
     });
 };
 
@@ -104,13 +177,10 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/focus', ({body: payload}, response) => {
-    treatPayload({user: 'Kleegroup', repo: 'focus'}, payload);
-    response.end();
-});
-
-app.post('/focus-components', ({body: payload}, response) => {
-    treatPayload({user: 'Kleegroup', repo: 'focus-components'}, payload);
+app.post('/', ({body: payload}, response) => {
+    if (payload) {
+        treatPayload(payload);
+    }
     response.end();
 });
 
