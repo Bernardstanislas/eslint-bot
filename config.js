4809c20100f67c7e8c14e42bd6372966c7bc6d43
Stanislas Bernard <bernardstanislas@gmail.com>
2015-09-06 23:20:36 +0200

    Add comments

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-export const GITHUB_USERNAME = 'username';
-export const GITHUB_PASSWORD = 'password';
+export const GITHUB_USERNAME = 'username'; // Your bot's Github username
+export const GITHUB_PASSWORD = 'password'; // Your bot's Github password
 
-export const REPOSITORY_OWNER = 'owner/organisation';
-export const REPOSITORY_NAME = 'name';
+export const REPOSITORY_OWNER = 'owner/organisation';   // The owner of the repository you want to run the bot on.
+export const REPOSITORY_NAME = 'name';                  // The name of the repository you want to run the bot on.
 
 export const FILE_FILTER = /.*(.js|.jsx)$/; // By default, lint every single .js or .jsx file
