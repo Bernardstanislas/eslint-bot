58f66a57dcb8dfbd60ba6cd4fa638d920057fd7e
Stanislas Bernard <bernardstanislas@gmail.com>
2015-09-06 23:15:14 +0200

    Add configuration file

---
+++ config.js
@@ -0,0 +1,7 @@
+export const GITHUB_USERNAME = 'username';
+export const GITHUB_PASSWORD = 'password';
+
+export const REPOSITORY_OWNER = 'owner/organisation';
+export const REPOSITORY_NAME = 'name';
+
+export const FILE_FILTER = /.*(.js|.jsx)$/; // By default, lint every single .js or .jsx file
